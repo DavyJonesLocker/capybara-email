2355db54cb5e61aa27e065562e79022287d94139
Matt Polito and Thais Camilo <dev+mattpolito+narwen@hashrocket.com>
2012-03-30 15:25:48 -0400

    Documentation for Capybara::Email::Driver

--- lib/capybara/email/driver.rb
+++ lib/capybara/email/driver.rb
@@ -17,30 +17,57 @@ class Capybara::Email::Driver < Capybara::Driver::Base
     Capybara.current_session.driver.visit url
   end
 
+
   def body
     dom.to_xml
   end
 
+  # Access to email subject
+  #
+  #  delegates back to instance of Mail::Message
+  #
+  # @return String
   def subject
     email.subject
   end
 
+  # Access to email recipient(s)
+  #
+  #  delegates back to instance of Mail::Message
+  #
+  # @return [Array<String>]
   def to
     email.to
   end
 
+  # Access to email sender(s)
+  #
+  #  delegates back to instance of Mail::Message
+  #
+  # @return [Array<String>]
   def from
     email.from
   end
 
+  # Nokogiri object for traversing content
+  #
+  # @return Nokogiri::HTML::Document
   def dom
     @dom ||= Nokogiri::HTML(source)
   end
 
+  # Find elements based on given xpath
+  #
+  # @param [xpath string]
+  #
+  # @return [Array<Capybara::Driver::Node>]
   def find(selector)
     dom.xpath(selector).map { |node| Capybara::Email::Node.new(self, node) }
   end
 
+  # String version of email HTML source
+  #
+  # @return String
   def source
     if email.mime_type == 'text/plain'
       convert_to_html(raw)
@@ -49,6 +76,9 @@ class Capybara::Email::Driver < Capybara::Driver::Base
     end
   end
 
+  # Plain text email contents
+  #
+  # @return String
   def raw
     email.body.encoded
   end
